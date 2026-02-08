8a24c048ea2f6efa9ef24cc17aba00ff1036fe02
Miracle A. <pleasureoverpain@gmail.com>
2026-02-08 00:32:13 +0000

    Update main.tf

--- aws-static-site-secure/terraform/main.tf
+++ aws-static-site-secure/terraform/main.tf
@@ -1,1 +1,16 @@
+provider "aws" {
+  region = "us-east-1"
+}
 
+resource "aws_s3_bucket" "site" {
+  bucket = "verdun90-secure-static-site"
+}
+
+resource "aws_s3_bucket_public_access_block" "block" {
+  bucket = aws_s3_bucket.site.id
+
+  block_public_acls   = true
+  block_public_policy = true
+  ignore_public_acls  = true
+  restrict_public_buckets = true
+}
