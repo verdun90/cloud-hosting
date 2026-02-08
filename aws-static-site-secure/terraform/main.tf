49e702db40b8438c4d6d756786ce61cc5db66360
Miracle A. <pleasureoverpain@gmail.com>
2026-02-08 07:17:21 +0000

    Update main.tf

--- aws-static-site-secure/terraform/main.tf
+++ aws-static-site-secure/terraform/main.tf
@@ -14,3 +14,11 @@ resource "aws_s3_bucket_public_access_block" "block" {
   ignore_public_acls  = true
   restrict_public_buckets = true
 }
+
+resource "aws_cloudfront_origin_access_control" "oac" {
+  name                              = "static-site-oac"
+  description                       = "OAC for private S3 bucket"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
